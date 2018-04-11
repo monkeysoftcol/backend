14f98b5d0e5252dfcfded6cc4669bd9f6eb53f79
dsalamanca <dsalamanca@10.10.254.180>
2018-04-11 08:21:18 -0500

    Se agregan tablas del modulo de administración

---
+++ Neighborhood/Neighborhood-ejb/src/main/java/co/com/monkey/dao/AdminSolicitudMudanzaDAO.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.com.monkey.dao;
+
+import javax.ejb.Stateless;
+
+/**
+ *
+ * @author dsalamanca
+ */
+@Stateless
+public class AdminSolicitudMudanzaDAO {
+    
+}
